17176d316e4c0ab94b06f18a086c3d177740d127
Vishalgoswami12 <vishalgoswami2250@gmail.com>
2022-08-30 23:14:22 +0530

    react-app

---
+++ block-BRaaao/gizmo/src/Components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <>
+        <header className="flex">
+            <h1>AltCampus</h1>
+            <nav>
+                <ul>
+                    <a class="anchor" href="#">Home</a>
+                    <a class="anchor" href="#">About</a>
+                    <a class="anchor" href="#">Contact</a>
+                </ul>
+            </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header;
